c4b9d6e0128da88f88fa83c06480578a154c6191
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2022-07-02 20:03:14 -0400

    added standstill ui

--- server.py
+++ server.py
@@ -36,36 +36,35 @@ class handler(BaseHTTPRequestHandler):
         elif self.path.startswith("/selectBag"):
             root = tk.Tk()
             root.withdraw()
-            msg = tk.filedialog.askopenfiles()
+            msg = tk.filedialog.askopenfiles(filetypes=[("Rosbag", "*.bag")])
             file_names = [file.name for file in msg]
             print(file_names)
             res(self, 200, "json", json.dumps(file_names))
 
         else:
             root = os.getcwd()
-            #print(self.path)
-            if self.path == '/':
-                filename = root + '/index.html'
+            # print(self.path)
+            if self.path == "/":
+                filename = root + "/index.html"
             else:
                 filename = root + self.path
-    
-            self.send_response(200)
-            if filename[-4:] == '.css':
-                self.send_header('Content-type', 'text/css')
-            elif filename[-5:] == '.json':
-                self.send_header('Content-type', 'application/javascript')
-            elif filename[-3:] == '.js':
-                self.send_header('Content-type', 'application/javascript')
-            elif filename[-4:] == '.ico':
-                self.send_header('Content-type', 'image/x-icon')
-            else:
-                self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            with open(filename, 'rb') as fh:
-                html = fh.read()
-                #html = bytes(html, 'utf8')
-                self.wfile.write(html)
 
+            self.send_response(200)
+            if filename[-4:] == ".css":
+                self.send_header("Content-type", "text/css")
+            elif filename[-5:] == ".json":
+                self.send_header("Content-type", "application/javascript")
+            elif filename[-3:] == ".js":
+                self.send_header("Content-type", "application/javascript")
+            elif filename[-4:] == ".ico":
+                self.send_header("Content-type", "image/x-icon")
+            else:
+                self.send_header("Content-type", "text/html")
+            self.end_headers()
+            with open(filename, "rb") as fh:
+                html = fh.read()
+                # html = bytes(html, 'utf8')
+                self.wfile.write(html)
 
 
 def startServer():
