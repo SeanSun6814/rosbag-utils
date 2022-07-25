94db9580c8cb3d479af5fbd4310fa6130a91051f
JonasJ-ap <rushanj@andrew.cmu.edu>
2022-07-25 01:59:53 -0400

    do not need to open the browser

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ import webbrowser
 thread = Thread(target = server.startServer)
 thread.start()
 print("Opening app in browser...")
-webbrowser.open('127.0.0.1:8000')
 thread.join()
 print("thread finished...exiting")
