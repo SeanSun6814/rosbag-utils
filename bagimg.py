5172da7af4bd083b6bc365a0a7b88e1cac548f1a
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2022-07-27 13:45:17 -0400

    basic video export

--- bagimg.py
+++ bagimg.py
@@ -1,14 +1,63 @@
+import rosbag
+import json
+import yaml
+import subprocess
+import traceback
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+textLocation = (10,30)
+fontScale = 1
+fontColor = (0,0,255)
+thickness = 2
+lineType = cv2.LINE_AA
 
 
+def formatTime(time, startime):
+    return str(round((time - startime)*1e-9, 3)) + "s"
 
-def exportVideo(paths, pathOut, topic, dt):
-    bagIn = rosbag.Bag(pathIn)
-    for topic, msg, t in bagIn.read_messages(topics=[topic]):
+def exportVideo(paths, pathOut, targetTopic, speed, printTimestamp):
+    bagIn = rosbag.Bag(paths)
+    bridge = CvBridge()
+    startTime = -1
+    video = None
+    
+    for topic, msg, t in bagIn.read_messages(topics=[targetTopic]):
+        if startTime == -1:
+            startTime = int(str(t))
+            video = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'mp4v'), 30, (msg.width,msg.height))
+            print("Video dimensions: " + str(msg.width) + "x" + str(msg.height))
+
         if "16UC1" in msg.encoding or "mono16" in msg.encoding:
             cv_img = np.array(bridge.imgmsg_to_cv2(msg))
             cv_img = (cv_img / 256).astype("uint8")
         elif "8UC1" in msg.encoding or "mono8" in msg.encoding:
             cv_img = np.array(bridge.imgmsg_to_cv2(msg, desired_encoding="mono8"))
-        elif "8UC3" in msg.encoding or "rgb8" in msg.encoding:
+        else:
             cv_img = np.array(bridge.imgmsg_to_cv2(msg))
-            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+
+        if (printTimestamp):
+            cv2.putText(cv_img, formatTime(int(str(t)), startTime), 
+                textLocation, 
+                font, 
+                fontScale,
+                fontColor,
+                thickness,
+                lineType)
+
+        video.write(cv_img)
+        cv2.imshow('Video preview', cv_img)
+        cv2.waitKey(1)
+
+    video.release()
+    cv2.destroyAllWindows()
+
+
+exportVideo("/media/sean/SSD/thermal_data/thermal_outside.bag",
+           "/media/sean/SSD/thermal_data/aaa.mp4",
+           "/thermal/image",
+           1.0, True
+           )
