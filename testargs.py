a079a2a1ddac9a9d8617f0aba1de5f5588973935
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2022-08-01 23:09:19 -0400

    cleaned up

--- testargs.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="Rosbag Utils")
-parser.add_argument("-n", "--no_browswer", action='store_true', help="Don't open browser by default.")
-parser.add_argument("-p", "--ports", nargs="*", default=[8000], help="Start multiple servers at different ports.")
-args = parser.parse_args()
-
-print(args)
