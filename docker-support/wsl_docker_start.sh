6273d76f722c379dc554aa08c0f50789447cbfe0
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2022-12-21 01:17:29 -0800

    updated wsl & linux docker start scripts

--- docker-support/wsl_docker_start.sh
+++ docker-support/wsl_docker_start.sh
@@ -1,6 +1,6 @@
 FOLDER="${1:-/home}"
 echo -e "Starting rosbag-utils with /data folder: \e[32m${FOLDER}\e[0m"
-echo -e "To change dataset folder, run \e[33m./docker_start.sh /your/folder/here\e[0m"
+echo -e "To change dataset folder, run \e[33m./wsl_docker_start.sh /your/folder/here\e[0m"
 
 docker run \
 -it \
@@ -8,7 +8,9 @@ docker run \
 -v $(readlink -f "../."):/root/rosbag-utils \
 -v "$FOLDER:/data" \
 -p 8000-8010:8000-8010 \
--e DISPLAY=$DISPLAY
+-e "DISPLAY" \
+-e "QT_X11_NO_MITSHM=1" \
+-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 --privileged \
 airlab/rosbag-utils:latest \
 /bin/bash -c "cd /root/rosbag-utils && python3 app.py -n"
