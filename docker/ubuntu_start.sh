38b21a71c8f819f128f8adbe72f6aadb3f610d47
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2023-03-21 22:14:05 -0400

    working on Ubuntu

--- docker/ubuntu_start.sh
+++ docker/ubuntu_start.sh
@@ -1,6 +1,14 @@
+if [ -z "$1" ]; then
+    echo -e "To change dataset folder, run \e[33m./docker_start.sh /your/folder/here\e[0m"
+fi
+
 FOLDER="${1:-/}"
 echo -e "Starting rosbag-utils with /data folder: \e[32m${FOLDER}\e[0m"
-echo -e "To change dataset folder, run \e[33m./docker_start.sh /your/folder/here\e[0m"
+
+# Install if necessary: sudo apt-get install x11-xserver-utils
+xhost + > /dev/null 2>&1
+
+(sleep 1 && xdg-open http://127.0.0.1:8000 > /dev/null 2>&1) &
 
 docker run \
 -it \
@@ -8,7 +16,8 @@ docker run \
 -v $(readlink -f "../."):/root/rosbag-utils \
 -v "$FOLDER:/data" \
 -p 8000-8001:8000-8001 \
--e DISPLAY=$DISPLAY
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=unix$DISPLAY \
 --privileged \
 rosbag-utils:latest \
 /bin/bash -c "cd /root/rosbag-utils && python3 app.py -n"
