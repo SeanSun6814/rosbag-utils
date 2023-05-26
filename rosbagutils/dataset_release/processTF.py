501ee99982319dae2cf3c499103ab992fc56b0fa
Damanpreet Singh <damanprs@andrew.cmu.edu>
2023-05-26 12:28:25 -0400

    Minor fix for processTF file

--- rosbagutils/dataset_release/processTF.py
+++ rosbagutils/dataset_release/processTF.py
@@ -1,14 +1,16 @@
 import rosbag
+import rostopic
 import random
 import pandas as pd
 from .. import utils
+from tqdm import tqdm
 
 def processTF(paths, targetTopic, pathOut, sendProgress):
-    utils.mkdir(utils.getFilenameFromPath(pathOut))
+    print("in Tf: ",pathOut)
+    utils.mkdir(utils.getFolderFromPath(pathOut))
     count = 0
     percentProgressPerBag = 1 / len(paths)
-    tf_df = pd.DataFrame()
-    tf_df.columns = ['timestamp','parent_frame','child_frame','trans_x','trans_y','trans_z','qx','qy','qz','qw']
+    tf_df = pd.DataFrame(columns = ['timestamp','parent_frame','child_frame','trans_x','trans_y','trans_z','qx','qy','qz','qw'])
     
     for path, pathIdx in zip(paths, range(len(paths))):
         if path.strip() == "":
@@ -31,33 +33,32 @@ def processTF(paths, targetTopic, pathOut, sendProgress):
         sendProgressEveryHowManyMessages = max(random.randint(77, 97), int(totalMessages / (100 / len(paths))))
         bagStartCount = count
     
-        for topic, msg, t in bagIn.read_messages(topics=[targetTopic]):
-            msg = msg[0]
-            
-            timestamp = float(msg.header.stamp)
-            parent_frame = str(msg.header.frame_id)
-            child_frame = str(msg.child_frame_id)
-            
-            trans_x = msg.transform.translation.x
-            trans_y = msg.transform.translation.y
-            trans_z = msg.transform.translation.z
-            
-            qx = msg.transform.rotation.x
-            qy = msg.transform.rotation.y
-            qz = msg.transform.rotation.z
-            qw = msg.transform.rotation.w
-            
-            tf_df.loc[len(tf_df.index)] = [timestamp,parent_frame,child_frame,trans_x,trans_y,trans_z,qx,qy,qz,qw]
-            
-            count += 1
-            if count % sendProgressEveryHowManyMessages == 0:
-                sendProgress(
-                    percentage=(
-                        basePercentage
-                        + ((count - bagStartCount) / totalMessages * 0.89 + 0.1) * percentProgressPerBag
-                    ),
-                    details=("Processing " + str(count) + " odometry messages"),
-                )
+        for topic, _msg, t in tqdm(bagIn.read_messages(topics=[targetTopic]) , total=totalMessages):
+            for msg in _msg.transforms:          
+                timestamp = float(msg.header.stamp.to_sec())
+                parent_frame = str(msg.header.frame_id)
+                child_frame = str(msg.child_frame_id)
+                
+                trans_x = msg.transform.translation.x
+                trans_y = msg.transform.translation.y
+                trans_z = msg.transform.translation.z
+                
+                qx = msg.transform.rotation.x
+                qy = msg.transform.rotation.y
+                qz = msg.transform.rotation.z
+                qw = msg.transform.rotation.w
+                
+                tf_df.loc[len(tf_df.index)] = [timestamp,parent_frame,child_frame,trans_x,trans_y,trans_z,qx,qy,qz,qw]
+                
+                count += 1
+                if count % sendProgressEveryHowManyMessages == 0:
+                    sendProgress(
+                        percentage=(
+                            basePercentage
+                            + ((count - bagStartCount) / totalMessages * 0.89 + 0.1) * percentProgressPerBag
+                        ),
+                        details=("Processing " + str(count) + " odometry messages"),
+                    )
     
     tf_df.to_csv(pathOut+'tf_data.csv')
     result = {
