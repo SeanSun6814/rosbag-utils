a96ad48cbce36e72c5b2215d8e6c5ca5448ae18b
Tianhao Wu <253750329@qq.com>
2023-06-12 22:03:16 -0400

    remove file writing in avg stat script

--- generateAvgStatistics.py
+++ generateAvgStatistics.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from tf.transformations import *
 
 
-def processOdometry(paths, targetTopic, pathOut, sendProgress=None):
+def processOdometry(paths, targetTopic):
     sum_a = 0
     sum_angular_rate = 0
     prev_p = [0.0, 0.0, 0.0]
@@ -17,95 +17,73 @@ def processOdometry(paths, targetTopic, pathOut, sendProgress=None):
     prev_timestamp = rospy.Duration(0.0)
     first_frame = True
     count = 0
-    percentProgressPerBag = 1 / len(paths)
-    with open(pathOut + "/odometry_data.csv", "w") as f:
-        f.write("timestamp, x, y, z, q_x, q_y, q_z, q_w\n")
-        for path, pathIdx in zip(paths, range(len(paths))):
-            if path.strip() == "":
-                continue
-            print("Processing   " + path)
-            bagIn = rosbag.Bag(path)
-            topicsInfo = bagIn.get_type_and_topic_info().topics
-            totalMessages = sum(
-                [topicsInfo[topic].message_count if topic in topicsInfo else 0 for topic in [
-                    targetTopic]]
+
+    for path, pathIdx in zip(paths, range(len(paths))):
+        if path.strip() == "":
+            continue
+        print("Processing   " + path)
+        bagIn = rosbag.Bag(path)
+        topicsInfo = bagIn.get_type_and_topic_info().topics
+        totalMessages = sum(
+            [topicsInfo[topic].message_count if topic in topicsInfo else 0 for topic in [
+                targetTopic]]
+        )
+
+        print("total messages:", totalMessages)
+        for topic, msg, t in tqdm(bagIn.read_messages(topics=[targetTopic]), total=totalMessages):
+
+            x, y, z = (
+                msg.pose.pose.position.x,
+                msg.pose.pose.position.y,
+                msg.pose.pose.position.z,
             )
 
-            print("total messages:", totalMessages)
-            for topic, msg, t in tqdm(bagIn.read_messages(topics=[targetTopic]), total=totalMessages):
-                timestamp = str(t)
+            q_x, q_y, q_z, q_w = (
+                msg.pose.pose.orientation.x,
+                msg.pose.pose.orientation.y,
+                msg.pose.pose.orientation.z,
+                msg.pose.pose.orientation.w,
+            )
 
-                x, y, z = (
-                    msg.pose.pose.position.x,
-                    msg.pose.pose.position.y,
-                    msg.pose.pose.position.z,
-                )
+            if first_frame:
+                prev_p = np.array([x, y, z])
+                prev_orientation_inv = np.array([-q_x, -q_y, -q_z, q_w])
+                prev_p = np.array([0.0, 0.0, 0.0], np.float64)
+                prev_timestamp = t
+                first_frame = False
+            else:
+                cur_p = np.array([x, y, z])
+                cur_orientation = np.array([q_x, q_y, q_z, q_w])
+                delta_t = (t-prev_timestamp).to_sec()
 
-                q_x, q_y, q_z, q_w = (
-                    msg.pose.pose.orientation.x,
-                    msg.pose.pose.orientation.y,
-                    msg.pose.pose.orientation.z,
-                    msg.pose.pose.orientation.w,
-                )
+                # if delta_t < 0.05:
+                # print(t)
+                # print(prev_timestamp)
+                # delta_t = 0.2
 
-                f.write(
-                    timestamp
-                    + ", "
-                    + str(x)
-                    + ", "
-                    + str(y)
-                    + ", "
-                    + str(z)
-                    + ", "
-                    + str(q_x)
-                    + ", "
-                    + str(q_y)
-                    + ", "
-                    + str(q_z)
-                    + ", "
-                    + str(q_w)
-                    + "\n"
-                )
+                cur_v = (cur_p - prev_p)/delta_t
+                cur_acc = (cur_v-prev_v)/delta_t
+                sum_a += np.linalg.norm(cur_acc)
 
-                if first_frame:
-                    prev_p = np.array([x, y, z])
-                    prev_orientation_inv = np.array([-q_x, -q_y, -q_z, q_w])
-                    prev_p = np.array([0.0, 0.0, 0.0], np.float64)
-                    prev_timestamp = t
-                    first_frame = False
-                else:
-                    cur_p = np.array([x, y, z])
-                    cur_orientation = np.array([q_x, q_y, q_z, q_w])
-                    delta_t = (t-prev_timestamp).to_sec()
+                delta_q = quaternion_multiply(
+                    prev_orientation_inv, cur_orientation)
+                if delta_q[3]-1 > 0:
+                    delta_q = np.array([0, 0, 0, 1])
+                delta_ang = np.arccos(delta_q[3])*2
 
-                    if delta_t < 0.05:
-                        # print(t)
-                        # print(prev_timestamp)
-                        delta_t = 0.2
+                if np.isnan(delta_ang):
+                    print("delta_q:{},{},{},{}".format(
+                        delta_q[0], delta_q[1], delta_q[2], delta_q[3]))
+                    print("delta_ang", delta_ang)
+                    exit(-1)
+                sum_angular_rate += delta_ang/delta_t
 
-                    cur_v = (cur_p - prev_p)/delta_t
-                    cur_acc = (cur_v-prev_v)/delta_t
-                    sum_a += np.linalg.norm(cur_acc)
+                prev_p = cur_p
+                prev_v = cur_v
+                prev_orientation_inv = [-q_x, -q_y, -q_z, q_w]
+                prev_timestamp = t
 
-                    delta_q = quaternion_multiply(
-                        prev_orientation_inv, cur_orientation)
-                    if delta_q[3]-1 > 0:
-                        delta_q = np.array([0, 0, 0, 1])
-                    delta_ang = np.arccos(delta_q[3])*2
-
-                    if np.isnan(delta_ang):
-                        print("delta_q:{},{},{},{}".format(
-                            delta_q[0], delta_q[1], delta_q[2], delta_q[3]))
-                        print("delta_ang", delta_ang)
-                        exit(-1)
-                    sum_angular_rate += delta_ang/delta_t
-
-                    prev_p = cur_p
-                    prev_v = cur_v
-                    prev_orientation_inv = [-q_x, -q_y, -q_z, q_w]
-                    prev_timestamp = t
-
-                count += 1
+            count += 1
 
     print("count:", count)
     avg_accelaration = sum_a/count
@@ -125,12 +103,12 @@ if __name__ == "__main__":
                                                 \ncausing unrealistic result")
     parser.add_argument('datapath', help='destination of data folder')
     parser.add_argument('--topic_name', default='/aft_mapped_to_init_imu',
-                        help="topic name that contains the ground truth")
+                        help="topic name that contains the ground truth odomtry")
 
     args = parser.parse_args()
 
     bags = os.listdir(args.datapath)
-    
+
     full_path_list = [os.path.join(args.datapath, x)
                       for x in bags if x.split(".")[-1] == 'bag']
-    processOdometry(sorted(full_path_list), args.topic_name, args.datapath)
+    processOdometry(sorted(full_path_list), args.topic_name)
